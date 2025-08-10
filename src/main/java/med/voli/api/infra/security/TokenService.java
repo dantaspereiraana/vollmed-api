a34ebcfb2c53fb7511c65513d63e858ed4a50b73
Ana Paula Dantas <dantaspereiraana@gmail.com>
2025-08-10 12:24:44 -0300

    Implementação da autorização com token (JSON Web Token);

---
+++ src/main/java/med/voli/api/infra/security/TokenService.java
@@ -0,0 +1,33 @@
+package med.voli.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voli.api.domain.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+    public String gerarToken(Usuario usuario){
+        //Modelo pego em https://github.com/auth0/java-jwt
+        try {
+            var algoritmo = Algorithm.HMAC256("12345678");
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
